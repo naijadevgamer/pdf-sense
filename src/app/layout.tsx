a9e4a5af2c6931132fccaf4bb1f46382928e4cf5
thenaijagamer <sabdullahialaba50@gmail.com>
2025-02-24 12:29:32 +0100

    Setup the providers in the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Sora } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
+import Providers from "@/components/Provider";
 
 const sora = Sora({
   variable: "--font-sora",
@@ -22,12 +23,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={cn("min-h-screen antialiased grainy", `${sora.className}`)}
-      >
-        <Navbar />
-        {children}
-      </body>
+      <Providers>
+        <body
+          className={cn("min-h-screen antialiased grainy", `${sora.className}`)}
+        >
+          <Navbar />
+          {children}
+        </body>
+      </Providers>
     </html>
   );
 }
