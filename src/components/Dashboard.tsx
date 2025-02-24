c452ffe48b1e183d732664b3a7f94dfcfa6a197c
thenaijagamer <sabdullahialaba50@gmail.com>
2025-02-24 18:37:32 +0100

    Start implementing the display of files

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import React from "react";
 import UploadButton from "./UploadButton";
+import { trpc } from "@/app/_trpc/client";
 
 const Dashboard = () => {
+  const { data: files, isLoading } = trpc.getUserFiles.useQuery();
+
   return (
     <main className="mx-auto max-w-7xl md:p-10">
       <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
@@ -11,6 +16,15 @@ const Dashboard = () => {
       </div>
 
       {/* display all user files */}
+      {!files || files.length === 0 ? (
+        <p className="mt-5 text-gray-500">No files uploaded yet</p>
+      ) : isLoading ? (
+        <p className="mt-5 text-gray-500">Loading...</p>
+      ) : (
+        <div className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+          {" "}
+        </div>
+      )}
     </main>
   );
 };
