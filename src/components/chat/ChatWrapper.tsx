9761dcf188ff5d2f591fcd0fdd598f9a672414ff
thenaijagamer <sabdullahialaba50@gmail.com>
2025-08-24 20:24:46 +0100

    Fix the distorted chat input display

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -102,8 +102,8 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
 
   return (
     <ChatContextProvider fileId={fileId}>
-      <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
-        <div className="flex-1 justify-between flex flex-col mb-28">
+      <div className="relative h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2 overflow-hidden">
+        <div className="flex-1 justify-between flex flex-col ">
           <Messages fileId={fileId} />
         </div>
         <ChatInput />
