0d64717f8f4939a57d1792e7615850ef0ca2d172
thenaijagamer <sabdullahialaba50@gmail.com>
2025-08-27 01:50:57 +0100

    Configure auth redirect

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/sign-in",
+        destination: "/api/auth/login",
+        permanent: true,
+      },
+      {
+        source: "/sign-up",
+        destination: "/api/auth/register",
+        permanent: true,
+      },
+      {
+        source: "/sign-out",
+        destination: "/api/auth/logout",
+        permanent: true,
+      },
+    ];
+  },
+
   /* config options here */
   experimental: {
     turbo: {
